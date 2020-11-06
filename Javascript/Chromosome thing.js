b3f47887d9807a24d0bd46c6769e8123864bedfc
lanoflatfaceo <lanoflatfaceo@gmail.com>
2020-11-06 09:29:09 -0500

    final version of spin off of Khan academy thing

--- Javascript/Chromosome thing.js
+++ Javascript/Chromosome thing.js
@@ -4,29 +4,39 @@ var isDog=false;
 var weightLB=16;
 var isCat=false;
 var isElephant=false;
+var isAlligator=true;
 
 //calculate weight in KG and RER (Resting Energy Requirement)
 var weightKG =weightLB/2.2;
 var restingEnergyReq=70* Math.pow(weightKG, 0.75);
 
 //Output daily caloric needs
-if (isDog==true) {
+if (isDog===true) {
   if (isNeutered) {
     println(1.6 * restingEnergyReq);
   } else{
     println(1.8 * restingEnergyReq);
   }
 
-} elseif(isCat==true) {//cat
+} elseif(isCat===true) {//cat
   if (isNeutered){
     println(1.2 * restingEnergyReq);
     }else{
       println(1.4 *restingEnergyReq);
     }
-} elseif(isElephant==true){
-
+} elseif(isElephant===true){
+    if(isNeutered) {
+      println(2.3 * restingEnergyReq);
+    } else {
+        println(2.5 * restingEnergyReq);
+    }
+} elseif(isAlligator){
+    if(isNeutered) {
+      println(2* restingEnergyReq);
+    } else{
+        println(2.2 * restingEnergyReq);
+    }
 }
 
 
 // elephants and lions
-if
