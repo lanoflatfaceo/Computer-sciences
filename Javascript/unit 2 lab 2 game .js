76d97d81be6a32fb9faa8da6a4325ec106eb58e9
lanoflatfaceo <43626792+lanoflatfaceo@users.noreply.github.com>
2020-12-02 08:50:21 -0500

    a mess

--- Javascript/unit 2 lab 2 game .js
+++ Javascript/unit 2 lab 2 game .js
@@ -1,6 +1,48 @@
+let input, button, greeting;
+
+function setup() {
+  // create canvas
+  createCanvas(710, 400);
+
+  input = createInput();
+  input.position(20, 65);
+
+  button = createButton('submit');
+  button.position(input.x + input.width, 65);
+  button.mousePressed(greet);
+
+  greeting = createElement('h2', 'What else do we need?');
+  greeting.position(20, 5);
+
+  textAlign(CENTER);
+  textSize(50);
+}
+
+
 //conversation between husband and wife about grocery shopping
  //Honey! Did you get the list?
  //yes, I have it.
- var shopping_list=["eggs", "potatoes", "etc."]
-var txt=input("What else do we need?")
- shopping_list.push("Milk")
+
+
+
+
+
+}
+
+function greet() {
+  push();
+  var conversation=["Honey! Did you get the list?", "Yes, I have it.", "Great, what else do we need?"];
+  var shoppinglist=["eggs", "potatoes", "etc."];
+  const txt=input.value("What else do we need?");
+  shoppinglist.push(txt);
+  greeting.html(shoppinglist[b]);
+for(var b=0; b<shoppinglist.length; b++){
+  text(shoppinglist[b], 0, 0);
+  pop();
+}
+
+for(var i =0; i<conversation.length; i++) {
+  print(conversation[i]);
+}
+
+}
