704a9dbfd8d0848f496920f7abea98cac7dab4f0
lanoflatfaceo <43626792+lanoflatfaceo@users.noreply.github.com>
2020-12-03 10:57:26 -0500

    final

--- Javascript/unit 2 lab 2 game .js
+++ Javascript/unit 2 lab 2 game .js
@@ -12,37 +12,45 @@ function setup() {
   button.mousePressed(greet);
 
   greeting = createElement('h2', 'What else do we need?');
+
+
   greeting.position(20, 5);
 
   textAlign(CENTER);
   textSize(50);
-}
+
 
 
 //conversation between husband and wife about grocery shopping
  //Honey! Did you get the list?
  //yes, I have it.
 
-
-
-
-
 }
 
 function greet() {
-  push();
+
   var conversation=["Honey! Did you get the list?", "Yes, I have it.", "Great, what else do we need?"];
-  var shoppinglist=["eggs", "potatoes", "etc."];
-  const txt=input.value("What else do we need?");
+  var shoppinglist=["eggs", "potatoes", "fish"];
+  const txt=input.value();
   shoppinglist.push(txt);
-  greeting.html(shoppinglist[b]);
-for(var b=0; b<shoppinglist.length; b++){
-  text(shoppinglist[b], 0, 0);
-  pop();
-}
+
+
 
 for(var i =0; i<conversation.length; i++) {
   print(conversation[i]);
+  a= createElement('p' , conversation[i]);
+//a.position(20,500);
+}
+for(var b=0; b<shoppinglist.length; b++){
+ // push();
+  print(shoppinglist[b]);
+
+  s = createElement('p' , shoppinglist[b]);
+//s.position(20,600);
+  //pop();
+
+
 }
 
+  print("Ok, let's go then");
 }
